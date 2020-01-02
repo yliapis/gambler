1727459818726908e60a18c8d75a55826311538a
yliapis <yliapis44@gmail.com>
2020-01-02 16:20:10 -0600

    Added contextual policy base

--- casino/policy/base.py
+++ casino/policy/base.py
@@ -1,5 +1,9 @@
 """
 Base class definitions for bandits
+
+TODO: find a way to marge Policy and ContextualPolicy
+to either be the same class, or inherit from the same
+class.
 """
 
 import numpy as np
@@ -62,5 +66,81 @@ class Policy(ABC):
             integer id of arm to reward
         reward : float
             size of reward
+        """
+        raise NotImplementedError
+
+
+class ContextualPolicy(ABC):
+
+    @abstractmethod
+    def _get_scores(self, context: np.array) -> np.array:
+        """
+        This is an array of reward scores for each arm. It may either
+        be drawn stochastically with each call or be deterministic.
+        This is used by the policy to select an arm.
+
+        Parameters
+        ----------
+        context : np.array of float
+            feature vector representing the context
+
+        Returns
+        -------
+        scores : np.array of float
+            array of scores for each arm.
+        """
+        raise NotImplementedError
+
+    def sample(self, context: np.array) -> int:
+        """
+        Draw an arm from the multi-arm bandit.
+
+        Parameters
+        ----------
+        context : np.array of float
+            feature vector representing the context
+
+        Returns
+        -------
+        arm : int
+            integer id of arm drawn
+        """
+        return np.argmax(self._get_scores(context))
+
+    def sample_k(self, k: int=1, context: np.array) -> np.array:
+        """
+        Draw multiple arms from the multi-arm bandit.
+
+        Parameters
+        ----------
+        context : np.array of float
+            feature vector representing the context
+
+        Returns
+        -------
+        arms : array of int
+            sequence of arms drawn in order of importance
+        """
+        return np.sorted(
+            np.argpartition(
+                -self._get_scores(context),
+                k=k,
+            )[:k],
+        )[::-1]
+
+    @abstractmethod
+    def reward(self, arm: int, context: np.array, reward: float=1.0) -> None:
+        """
+        Set a reward for a given arm. In the case of the bernoulli bandit,
+        a miss can be a reward of 0, and a hit can be a reward of 1.
+
+        Parameters
+        ----------
+        arm : int
+            integer id of arm to reward
+        reward : float
+            size of reward
+        context : np.array of float
+            feature vector representing the context
         """
         raise NotImplementedError
