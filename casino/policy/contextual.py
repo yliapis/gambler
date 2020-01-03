73b0c308346fc69e6efe6d5aab24baefdef81c4b
yliapis <yliapis44@gmail.com>
2020-01-02 19:28:44 -0600

    Implemented AdPredictor details

--- casino/policy/contextual.py
+++ casino/policy/contextual.py
@@ -94,11 +94,75 @@ class AdPredictor(ContextualPolicy):
             Advertising in Microsoft’s Bing Search Engine
         https://www.microsoft.com/en-us/research/wp-content/uploads/
             2010/06/AdPredictor-ICML-2010-final.pdf
+
+    NOTE:
+        * each arm has its own weight vector... this is an assumption,
+        need to double check the paper's implementation strategy
+        * original paper is for CTR estimation, the authors
+        represent a click as +1 and no click as -1, vs click as +1
+        and no click as 0
+        * input features (context) need to be normalized
     """
     def __init__(
         self,
         n_arms: int,
-        context_dim: Optional[int]=None
+        beta: float,
+        context_dim: Optional[int]=None,
     ) -> None:
 
-    raise NotImplementedError("WIP")
+        self.n_arms = n_arms
+
+        self._beta = beta
+
+        if context_dim:
+            self.__init_algo(context_dim)
+
+    def __init_algo(self, context_dim: int) -> None:
+        self._context_dim = context_dim
+
+        # initialize weight beliefs... lets set
+        # the default to zero mean, unit variance
+        self.mu = np.zeros((self.n_arms, context_dim))
+        self.sig2 = np.ones((self.n_arms, context_dim))
+
+    def __w(self, x: float):
+        v = self.__v(x)
+        return v * (x + v)
+
+    def __v(self, x: float):
+        return stats.norm.pdf(x) / stats.norm.cdf(x)
+
+    def _get_scores(self, context: np.array) -> np.array:
+
+        if not hasattr(self, "_context_dim"):
+            self.__init_algo(len(context))
+
+        x = context
+        beta = self._beta
+
+        w = stats.norm.rvs(loc=self.mu, scale=self.sig2)
+
+        return stats.norm.cdf(w @ x / beta)
+
+    def reward(
+        self,
+        arm: int,
+        context: np.array,
+        reward: float=1.0
+    ) -> None:
+
+        # update
+        beta = self._beta
+        x = context
+        y = reward
+        mu = self.mu[arm]
+        sig2 = self.sig2[arm]
+
+        Sigma = beta*2 + x @ sig2
+
+        mu_next = mu + y * x * (sig2 / Sigma) * self.__v(y * x @ mu / Sigma)
+        sig2_next = sig2 * (1 - x * (sig2 / Sigma) * self.__w(y * x @ mu / Sigma)
+
+        # set the updated values internally
+        self.mu[arm] = mu_next
+        self.sig2[arm] = sig2_next
