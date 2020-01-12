2fd4fdeadbe418b2046415995e41ca8fc7507d8d
yliapis <yliapis44@gmail.com>
2020-01-12 16:37:24 -0600

    Added contextual policy support in Agent

--- gambler/agent.py
+++ gambler/agent.py
@@ -1,15 +1,18 @@
 
 from typing import Any, Dict, Optional, Tuple
 
-import gambler.policy
+from gambler import policy
+from gambler.policy.base import is_contextual
 
 
 POLICY_TABLE = {
-    "epsilon_greedy": gambler.policy.EpsilonGreedy,
-    "exp3": gambler.policy.Exp3,
-    "random": gambler.policy.Random,
-    "thompson_sampling": gambler.policy.ThompsonSampling,
-    "ucb1": gambler.policy.UCB1,
+    "epsilon_greedy": policy.EpsilonGreedy,
+    "exp3": policy.Exp3,
+    "random": policy.Random,
+    "thompson_sampling": policy.ThompsonSampling,
+    "ucb1": policy.UCB1,
+    "linucb": policy.LinUCB,
+    "adpredictor": policy.AdPredictor,
 }
 
 
@@ -30,14 +33,33 @@ class Agent:
         self.n_draws = 0
         self.total_reward = 0
 
-    def draw(self) -> int:
+    def draw(
+        self,
+        context: Optional[np.ndarray]=None
+        ) -> int:
+
+        if is_contextual(self.policy):
+            arm = self.policy.sample(context=context)
+        else:        
+            arm = self.policy.sample()
+
         self.n_draws += 1
-        arm = self.policy.sample()
         self.last_drawn = arm
+
         return arm
 
-    def reward(self, reward: float=0.0, arm: Optional[int]=None) -> None:
+    def reward(
+        self,
+        reward: float=0.0,
+        arm: Optional[int]=None,
+        context: Optional[np.ndarray]=None
+    ) -> None:
+
         self.total_reward += reward
         if arm is None:
             arm = self.last_drawn
-        self.policy.reward(arm, reward)
+
+        if is_contextual(self.policy):
+            self.policy.reward(arm, reward, context=context)
+        else:
+            self.policy.reward(arm, reward)
